1c8e95535076a85fb2db105b3366a20f37c1c7cf
sajed-evsp <sajed@almochat.com>
2025-06-22 20:53:08 +0530

    first commit

---
+++ src/lib/route-config.ts
@@ -0,0 +1,5 @@
+import Routes from "@/routes/routes";
+import { createBrowserRouter } from "react-router-dom";
+
+const routes = Routes();
+export const router = createBrowserRouter(routes);
---
+++ src/routes/routes.tsx
@@ -0,0 +1,10 @@
+import Landing from "@/app/landing/pages/Landing";
+
+const Routes = () => [
+    {
+        path: "/",
+        element: <Landing />
+    }
+];
+
+export default Routes
