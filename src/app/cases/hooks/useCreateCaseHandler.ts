07c889de2b3ca65b6def9424195eb0adcb2b1b91
sajed-evsp <sajed@almochat.com>
2025-06-30 18:32:01 +0530

    add case creation handler to invalidate ticket queries and clear local storage

--- src/app/cases/hooks/useCreateCaseHandler.ts
+++ src/app/cases/hooks/useCreateCaseHandler.ts
@@ -1,14 +1,18 @@
-import { useAuthStore } from "@/store/auth-store";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createCase } from "../api/case-api";
-import type { ICase } from "../types/case-types";
+import {
+  FMD_CLIENT_QUERY_LOCAL_STORAGE_KEY,
+  type ICase,
+} from "../types/case-types";
 import { toast } from "sonner";
 import { useNavigate } from "react-router-dom";
 import siteLinks from "@/lib/sitelinks";
+import { useAuthStore } from "@/store/auth-store";
 
 const useCreateCaseHandler = () => {
   const { user } = useAuthStore();
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: createCaseMutate, isPending } = useMutation({
     mutationKey: ["CREATE_A_CASE", user?._id],
@@ -19,6 +23,13 @@ const useCreateCaseHandler = () => {
       if (data?.data?.success) {
         toast.success("Case created successfully!");
         const createdCase = data?.data?.data as ICase;
+
+        queryClient.invalidateQueries({
+          queryKey: ["GET_ALL_TICKETS", user?._id],
+        });
+
+        localStorage.removeItem(FMD_CLIENT_QUERY_LOCAL_STORAGE_KEY);
+
         navigate(siteLinks.caseDetails(createdCase?._id).link);
       } else {
         toast.error("Failed to create a case");
