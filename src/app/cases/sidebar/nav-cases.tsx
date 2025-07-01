98187522330fb7fae2e6c18bcd5181efd9df1a13
sajed-evsp <sajed@almochat.com>
2025-07-01 22:31:34 +0530

    refactor NavCases component to utilize ShowCaseList for case display; remove unused imports and dropdown menu functionality

--- src/app/cases/sidebar/nav-cases.tsx
+++ src/app/cases/sidebar/nav-cases.tsx
@@ -1,37 +1,14 @@
-import {
-  ArrowUpRight,
-  Copy,
-  MoreHorizontal,
-  StarOff,
-  Trash2,
-} from "lucide-react"
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import {
-  SidebarGroup,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuAction,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  useSidebar,
-} from "@/components/ui/sidebar"
 import InlineEmpty from "@/components/custom/InlineEmpty"
 import useCaseGetter from "../hooks/useCaseGetter"
-import siteLinks from "@/lib/sitelinks"
 import InlineLoading from "@/components/custom/InlineLoading"
 import ErrorState from "@/components/custom/ErrorState"
-import { Link } from "react-router-dom"
+import ShowCaseList from "../components/ShowCaseList"
+import { SidebarGroup, SidebarGroupLabel, SidebarMenu } from "@/components/ui/sidebar"
 
 export function NavCases() {
-  const { isMobile } = useSidebar()
   const { cases, isError, isLoading } = useCaseGetter()
+
 
   return (
     <SidebarGroup className="group-data-[collapsible=icon]:hidden">
@@ -43,47 +20,7 @@ export function NavCases() {
             : isError
               ? <ErrorState className="mt-10" title="Failed to fetch cases" />
               : cases?.length > 0
-                ? cases.map((item) => (
-                  <SidebarMenuItem key={item._id}>
-                    <SidebarMenuButton asChild>
-                      <Link to={siteLinks.caseDetails(item?._id).link}>
-                        <span>{item.ai_case_name}</span>
-                      </Link>
-                    </SidebarMenuButton>
-                    <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <SidebarMenuAction showOnHover>
-                          <MoreHorizontal />
-                          <span className="sr-only">More</span>
-                        </SidebarMenuAction>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent
-                        className="w-56 rounded-lg"
-                        side={isMobile ? "bottom" : "right"}
-                        align={isMobile ? "end" : "start"}
-                      >
-                        <DropdownMenuItem>
-                          <StarOff className="text-muted-foreground" />
-                          <span>Remove from Favorites</span>
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>
-                          <Copy className="text-muted-foreground" />
-                          <span>Copy Link</span>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                          <ArrowUpRight className="text-muted-foreground" />
-                          <span>Open in New Tab</span>
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>
-                          <Trash2 className="text-muted-foreground" />
-                          <span>Delete</span>
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </SidebarMenuItem>
-                ))
+                ? <ShowCaseList cases={cases} />
                 : <div className="flex flex-col item-center gap-4">
                   <InlineEmpty title="No cases found..." className="mt-10" />
                 </div>
